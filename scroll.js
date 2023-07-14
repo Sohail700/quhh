ff2706c666b2528574a7868f9ea352084cbfea33
sohail khan <sk22222223@gmail.com>
2023-07-14 16:05:38 +0530

    modified:   about.css  Changes not staged for commit: 	modified:   common.css

--- scroll.js
+++ scroll.js
@@ -34,3 +34,13 @@ $('a[href*="#"]')
       }
     }
   });
+
+  $("a[href^='#']").click(function(e) {
+    e.preventDefault();
+    
+    var position = $($(this).attr("href")).offset().top;
+  
+    $("body, html").animate({
+      scrollTop: position
+    } /* speed */ );
+  });
